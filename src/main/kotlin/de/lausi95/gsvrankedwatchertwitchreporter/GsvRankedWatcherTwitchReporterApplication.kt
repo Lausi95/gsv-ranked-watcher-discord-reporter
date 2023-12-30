7580d051b20decf7fdbe095e8e6ad0b2398913c5
Tom Lausmann <tomlausmann@gmail.com>
2023-12-30 13:36:04 +0100

    Initial Commit

---
+++ src/main/kotlin/de/lausi95/gsvrankedwatchertwitchreporter/GsvRankedWatcherTwitchReporterApplication.kt
@@ -0,0 +1,11 @@
+package de.lausi95.gsvrankedwatchertwitchreporter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GsvRankedWatcherTwitchReporterApplication
+
+fun main(args: Array<String>) {
+  runApplication<GsvRankedWatcherTwitchReporterApplication>(*args)
+}
---
+++ src/test/kotlin/de/lausi95/gsvrankedwatchertwitchreporter/GsvRankedWatcherTwitchReporterApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.lausi95.gsvrankedwatchertwitchreporter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GsvRankedWatcherTwitchReporterApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
