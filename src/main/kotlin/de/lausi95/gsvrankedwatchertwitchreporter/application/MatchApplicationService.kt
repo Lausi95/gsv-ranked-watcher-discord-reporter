d5bce5c1402ea1deeced2876f79ac4e34fed0bf2
Tom Lausmann <tom.lausmann@unitb.de>
2024-01-08 11:00:43 +0100

    chore: add logging to reported match

--- src/main/kotlin/de/lausi95/gsvrankedwatchertwitchreporter/application/MatchApplicationService.kt
+++ src/main/kotlin/de/lausi95/gsvrankedwatchertwitchreporter/application/MatchApplicationService.kt
@@ -2,12 +2,16 @@ package de.lausi95.gsvrankedwatchertwitchreporter.application
 
 import de.lausi95.gsvrankedwatchertwitchreporter.domain.model.match.Match
 import de.lausi95.gsvrankedwatchertwitchreporter.domain.service.MatchReporter
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class MatchApplicationService(private val matchReporter: MatchReporter) {
 
+  private val log = LoggerFactory.getLogger(MatchApplicationService::class.java)
+
   fun reportMatch(match: Match) {
+    log.info("Reporting Match: $match")
     matchReporter.reportMatch(match)
   }
 }
